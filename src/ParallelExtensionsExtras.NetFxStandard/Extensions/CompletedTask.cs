df9bfe43f85f55322dab581e6e9ec0dfd90cea12
Sergey Volkov <ser121512@yandex.ru>
2023-12-17 23:27:33 +0300

    Allow CompletedTask<TResult> to be used outside of .NET Framework
    And use optimized implementation in newer .NET versions.

--- src/ParallelExtensionsExtras.NetFxStandard/Extensions/CompletedTask.cs
+++ src/ParallelExtensionsExtras.NetFxStandard/Extensions/CompletedTask.cs
@@ -7,7 +7,7 @@
 //--------------------------------------------------------------------------
 
 namespace System.Threading.Tasks;
-#if NET40 || NET45
+#if !NET46_OR_GREATER && !NETSTANDARD1_3_OR_GREATER && !NETCOREAPP
 /// <summary>Provides access to an already completed task.</summary>
 /// <remarks>A completed task can be useful for using <see cref="Task"/>.ContinueWith overloads where there aren't <see cref="TaskFactory"/>.StartNew equivalents.</remarks>
 public static class CompletedTask
@@ -15,6 +15,7 @@ public static class CompletedTask
     /// <summary>Gets a completed <see cref="Task"/>.</summary>
     public readonly static Task Default = CompletedTask<object>.Default;
 }
+#endif
 
 /// <summary>Provides access to an already completed task.</summary>
 /// <remarks>A completed task can be useful for using <see cref="Task"/>.ContinueWith overloads where there aren't <see cref="TaskFactory"/>.StartNew equivalents.</remarks>
@@ -23,12 +24,15 @@ public static class CompletedTask<TResult>
     /// <summary>Initializes a <see cref="Task{TResult}"/>.</summary>
     static CompletedTask()
     {
+#if NET45_OR_GREATER || NETSTANDARD || NETCOREAPP
+        Default = Task.FromResult(default(TResult));
+#else
         var tcs = new TaskCompletionSource<TResult?>();
         tcs.TrySetResult(default(TResult));
         Default = tcs.Task;
+#endif
     }
 
     /// <summary>Gets a completed <see cref="Task{TResult}"/>.</summary>
     public readonly static Task<TResult?> Default;
 }
-#endif
